60889aa91c97c4670b3c5bdbbe97067c7e333829
Rehan <rbashir2001@gmail.com>
2022-11-13 12:53:18 +0500

    Homepage Test Cases commit.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+//Tests for homepage
+
+test('Check if homepage is loaded..', () => {
   render(<App />);
-  const linkElement = screen.getByTestId("homepage");
+  const linkElement = screen.getByTestId("loading");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Check Admin button appeared..', () => {
+  render(<App/>);
+  const linkElement = screen.getByTestId("Admin Button");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Check Student button appeared..', () => {
+  render(<App/>);
+  const linkElement = screen.getByTestId("Student Button");
   expect(linkElement).toBeInTheDocument();
 });
