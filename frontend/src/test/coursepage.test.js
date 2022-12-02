c25a1c7ec0c32be9d996f3806db89a099795da86
Rehan <rbashir2001@gmail.com>
2022-12-03 01:57:17 +0500

    File Structure Changed. Test Files Fixed

--- frontend/src/test/coursepage.test.js
+++ frontend/src/test/coursepage.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import CoursePage from './Components/coursePage';
+import CoursePage from '../Components/coursePage';
 import {BrowserRouter} from 'react-router-dom';
 
 test('Check course list loaded..', () => {
--- frontend/src/test/navbar.test.js
+++ frontend/src/test/navbar.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import NavBar from './Components/Navbar';
+import NavBar from '../Components/Navbar';
 import {BrowserRouter} from 'react-router-dom';
 
 test("Check if home button loaded..", () => {
